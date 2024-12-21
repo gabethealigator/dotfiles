976c09f22001f3c28f50d8274dfbdc326b6707b8
gabrielmslima <gabrielnunesinfor@gmail.com>
2024-12-21 17:52:14 -0300

    nem sei

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,9 +3,11 @@ set fish_greeting ""
 set -gx EDITOR nvim
 
 set -gx PATH bin $PATH
+set -gx PATH ~/.bun/bin/ $PATH
 set -gx PATH ~/bin $PATH
 set -gx bin $PATH ~/.local/bin $PATH
 
 alias ll "exa -l -g --icons"
 alias lla "ll -a"
 alias g "git"
+alias t "tmux-sessionizer"

619590f50cb4d1c05e9cca87c8047c88e23048dd
gabrielmslima <gabrielnunesinfor@gmail.com>
2025-01-28 20:24:58 -0300

    preguica de escrever commit

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -12,6 +12,11 @@ set -gx bin $PATH ~/.local/bin $PATH
 set -gx PATH ~/Android/Sdk/cmdline-tools/latest/bin/ $PATH
 set -gx PATH ~/Android/Sdk/platform-tools $PATH
 set -gx PATH ~/Android/Sdk/platforms/android-35 $PATH
+set -gx JAVA_HOME /usr/bin/java $JAVA_HOME
+
+set --export JAVA_HOME (dirname (dirname (readlink -f (which java))))
+set -gx PATH $JAVA_HOME $PATH
+
 
 alias ll "exa -l -g --icons"
 alias lla "ll -a"
@@ -21,3 +26,6 @@ alias ta "tmux attach"
 alias tk "tmux kill-server"
 alias tl "tmux list-seesions"
 alias ai "tgpt"
+
+# Created by `pipx` on 2025-01-13 17:33:53
+set PATH $PATH /home/gabriel/.local/bin
